49b0aca5abffb607b00301cbfc8f8838b3ff85af
Misael Taveras <hello@taverasmisael.com>
2023-04-25 18:00:53 -0400

    commit to the i18n thing. no "changes" just adopting the new modality

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,7 +1,23 @@
-export const DEFAULT_LOCALE = "es";
+import locales, { DEFAULT_LOCALE, type LocaleKey } from "@/i18n";
 
 export function getFullLocalizedPath(locale: string) {
   return (path: string): string => {
     return `/${locale || DEFAULT_LOCALE}${path}/`;
   };
 }
+
+export function getLangFromUrl(url: URL, withFallback?: boolean): string {
+  const [, lang] = url.pathname.split("/");
+
+  if (withFallback) {
+    return isSupportedLang(lang) ? lang : DEFAULT_LOCALE;
+  }
+
+  return lang;
+}
+
+export function isSupportedLang(lang: string): lang is LocaleKey {
+  return lang in locales;
+}
+
+export { DEFAULT_LOCALE };
