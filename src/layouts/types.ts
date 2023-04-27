95940e66c2dffdc418a76c2c05d6a2aa7afa62f3
Misael Taveras <hello@taverasmisael.com>
2023-04-27 14:42:31 -0400

    add canonical url as a prop for layouts

--- src/layouts/types.ts
+++ src/layouts/types.ts
@@ -1,6 +1,7 @@
 import type {AstroBuiltinAttributes} from 'astro'
 export interface DefaultLayoutProps {
   title: string;
+  canonical: string;
   lang?: string;
   description: string;
   class?: string;
