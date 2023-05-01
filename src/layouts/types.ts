18a9dcffe8035cde5a67130715361dbccd744bef
Misael Taveras <hello@taverasmisael.com>
2023-05-01 10:32:54 -0400

    fix header/footer lang (pass the prop from layouts)

--- src/layouts/types.ts
+++ src/layouts/types.ts
@@ -1,8 +1,9 @@
+import type { Language } from '@/i18n';
 import type {AstroBuiltinAttributes} from 'astro'
 export interface DefaultLayoutProps {
   title: string;
   canonical: string;
-  lang?: string;
+  lang?: Language;
   description: string;
   class?: string;
   "class:list"?: AstroBuiltinAttributes['class:list'];
