7b11a531c067f626470d51e0ed8664eb327431ff
Misael Taveras <hello@taverasmisael.com>
2023-05-04 11:29:23 -0400

    remove full height on mobile for About page

--- src/reset.d.ts
+++ src/reset.d.ts
@@ -1,17 +1,1 @@
-/// <reference types="./env.d.ts" />
-import "@total-typescript/ts-reset";
-import { z } from "zod";
-
-const env = z.object({
-  PUBLIC_SITE_URL: z.string(),
-});
-
-export type Env = z.infer<typeof env>;
-
-declare global {
-  type ImportMetaEnv = Env;
-
-  interface ImportMeta {
-    env: ImportMetaEnv;
-  }
-}
+ import "@total-typescript/ts-reset";
