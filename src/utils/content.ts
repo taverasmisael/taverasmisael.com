da2f5c8e58b992a032bf57e10f8892dd62c2176b
Misael Taveras <hello@taverasmisael.com>
2023-05-04 11:18:56 -0400

    crearte RecentEntries compoenent and add it to about page

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,38 +1,34 @@
 import { getCollection, getEntryBySlug, type CollectionEntry } from "astro:content";
 import { type Language, DEFAULT_LOCALE } from "./i18n";
-import { SITE } from "@/config";
+
+const SITE_URL = import.meta.env.PUBLIC_SITE_URL as string;
 
 const Collections = {
   blog: "blog",
 } as const satisfies Record<string, Parameters<typeof getCollection>["0"]>;
 
 export type CollectionKey = keyof typeof Collections;
-
+export type Entry = BlogEntry;
 export function getCollectionName(key: string): undefined | (typeof Collections)[CollectionKey] {
   return Collections[key as CollectionKey];
 }
 
-export async function getBlogEntry(slug: string): Promise<BlogEntry | undefined> {
+export async function blogCollectionToBlogEntry(entry: CollectionEntry<"blog">, slug: string): Promise<BlogEntry> {
   const [lang, rawSlug] = slug.split("/") as [Language, string];
-  const entry = await getEntryBySlug("blog", slug);
 
-  if (!entry) return;
-  const translations: EntryTranslationReference[] = (
-    await getCollection("blog", p => {
-      const [pLang, pSlug] = p.slug.split("/") as [Language, string];
-      return pLang !== lang && pSlug === rawSlug;
-    })
-  ).map(p => {
+  const translationsEntities = await getCollection("blog", p => {
+    const [pLang, pSlug] = p.slug.split("/") as [Language, string];
+    return pLang !== lang && pSlug === rawSlug;
+  });
+
+  const translations = translationsEntities.map(p => {
     const pLang = p.slug.split("/")[0] as Language;
-    return {
-      lang: pLang,
-      slug: getEntryURL("blog", p.slug),
-      isOriginal: pLang === DEFAULT_LOCALE,
-    };
+    return { lang: pLang, slug: getEntryURL("blog", p.slug), isOriginal: pLang === DEFAULT_LOCALE };
   });
 
   const meta: BlogEntryMeta = {
     lang,
+    formattedDate: entry.data.date.toLocaleDateString(lang, { year: "numeric", month: "long", day: "numeric" }),
     title: entry.data.title,
     description: entry.data.description,
     canonical: slugToCanonical(getEntryURL("blog", entry.slug)),
@@ -48,6 +44,27 @@ export async function getBlogEntry(slug: string): Promise<BlogEntry | undefined>
   };
 }
 
+export async function getBlogEntry(slug: string): Promise<BlogEntry | undefined> {
+  const entry = await getEntryBySlug("blog", slug);
+
+  if (!entry) return;
+  return blogCollectionToBlogEntry(entry, slug);
+}
+
+export async function getBlogEntriesByLang(lang: Language): Promise<BlogEntry[]> {
+  const entries = await getCollection("blog", p => p.slug.startsWith(`${lang}/`));
+  return Promise.all(entries.map(e => blogCollectionToBlogEntry(e, e.slug)));
+}
+
+export function getEntriesByLang(collection: CollectionKey, lang: Language): Promise<Entry[]> {
+  switch (collection) {
+    case "blog":
+      return getBlogEntriesByLang(lang);
+    default:
+      throw new Error(`Invalid collection name: ${collection as string}`);
+  }
+}
+
 export function getEntryURL(entryType: CollectionKey, slug: string): string {
   const collection = getCollectionName(entryType);
 
@@ -57,7 +74,7 @@ export function getEntryURL(entryType: CollectionKey, slug: string): string {
   return `/${lang}/${collection}/${rawSlug}`;
 }
 
-export function slugToCanonical(slug: string, base: string = SITE.url): string {
+export function slugToCanonical(slug: string, base: string = SITE_URL): string {
   return new URL(slug, base).toString();
 }
 
@@ -76,6 +93,7 @@ export interface EntryTranslationReference {
 
 export interface BlogEntryMeta {
   canonical: string;
+  formattedDate: string;
   description: string;
   lang: Language;
   ogTitle: string;
