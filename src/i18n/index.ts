7d987bf8f0a9f385cd038cca0a4f829753c4b472
Misael Taveras <hello@taverasmisael.com>
2023-05-05 18:52:46 -0400

    add rss feed link to footer, improve nav links logic and header HOME link

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -15,7 +15,7 @@ export const locales = {
   en: English,
 } as const satisfies LocaleSet;
 
-export const PAGE_NAMES = ["blog", "about", "contact"] as const;
+export const PAGE_NAMES = ["home", "blog", "about", "contact", "feed"] as const;
 export type PageName = (typeof PAGE_NAMES)[number];
 type PageURL = Record<PageName, Readonly<string>>;
 
@@ -23,9 +23,11 @@ export const PAGE_URLS = {
   // This might look silly now, but maybe in the future the keys and values will be different.
   // For example, if we want to have a "promo23" page, we can have it as a key, but the URL
   // can be "promo-2023" or something like that.
-  blog: 'blog',
-  about: 'about',
-  contact: 'contact',
+  home: "",
+  blog: "blog",
+  about: "about",
+  contact: "contact",
+  feed: "rss.xml",
 } as const satisfies PageURL;
 
 export type LocaleDomain = keyof BaseLocale;
