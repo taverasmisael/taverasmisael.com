da1792c7acf9f4c0925883eecbac18ce1bf4d626
Misael Taveras <hello@taverasmisael.com>
2023-05-08 18:37:01 -0400

    create entries sitemap "properly"

---
+++ src/utils/entriesSitemap.ts
@@ -0,0 +1,33 @@
+import { getCollection, getEntryURL } from "@/utils/content";
+import { SitemapStream, EnumChangefreq, type SitemapItemLoose, streamToPromise } from "sitemap";
+import { Readable } from "stream";
+import type { Language } from "@/utils/i18n";
+
+export const prerender = true;
+
+type SitemapItem = Pick<SitemapItemLoose, "url" | "lastmod" | "changefreq" | "priority" | "links">;
+
+export async function createEntriesSitemap(hostname: string) {
+  const sitemapStream = new SitemapStream({ hostname });
+  const blogCollection = await getCollection("blog");
+  const blogEntries = blogCollection.map(e => {
+    const [lang, slug] = e.slug.split("/") as [Language, string];
+    return { lang, slug, url: getEntryURL("blog", e.slug), date: e.data.date.toISOString() };
+  });
+
+  const blogItems = blogEntries.map<SitemapItem>(entry => {
+    const { lang, slug } = entry;
+
+    const translationsEntities = blogEntries.filter(e => e.lang !== lang && e.slug === slug);
+
+    return {
+      ...entry,
+      lastmod: entry.date, // TODO: get lastmod from git
+      changefreq: EnumChangefreq.MONTHLY,
+      priority: 0.6,
+      links: translationsEntities,
+    } satisfies SitemapItem;
+  });
+
+  return streamToPromise(Readable.from(blogItems).pipe(sitemapStream));
+}
