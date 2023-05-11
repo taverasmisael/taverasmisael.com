97027c61d43ce0f4fa8e527040affe7bebd0d47a
Misael Taveras <hello@taverasmisael.com>
2023-05-11 11:08:43 -0400

    add node adapter for local previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,20 @@ import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import solidjs from "@astrojs/solid-js";
 import netlify from "@astrojs/netlify/functions";
+import node from "@astrojs/node";
 
 import { sitemap } from "./integrations/sitemap";
 
 const env = loadEnv(import.meta.env.MODE, process.cwd(), "") || process.env;
 
+// Netlify adapter doesn't support SSR yet, so we use node adapter for local builds
+// This is useful for testing SSR locally
+const adapter = env.LOCAL_BUILD ? node({ mode: "standalone" }) : netlify();
+console.log("Using adapter:", adapter.name);
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter,
   experimental: { assets: true },
   integrations: [
     tailwind(),
