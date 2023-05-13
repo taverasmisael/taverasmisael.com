fb359985b36d954563e0e9323305af5733d372d6
Misael Taveras <hello@taverasmisael.com>
2023-05-13 11:10:11 -0400

    add mailgun envvars

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 
 const envSchema = z.object({
   SITE: z.string(),
+  PUBLIC_SITE_URL: z.string().nonempty(),
+  MAILGUN_DOMAIN: z.string().nonempty(),
+  MAILGUN_API_KEY: z.string().nonempty(),
+  CONTACT_EMAIL: z.string().email(),
 });
 
 export type Env = z.infer<typeof envSchema>;
