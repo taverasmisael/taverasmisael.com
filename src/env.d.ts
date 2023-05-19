dc289119ae33066a4aba96c7c80704a72627909a
Misael Taveras <hello@taverasmisael.com>
2023-05-19 17:27:55 -0400

    add better css modules support

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,13 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
+
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: Record<string, string>;
+  export default classes;
+}
