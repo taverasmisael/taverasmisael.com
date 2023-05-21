7c403bacafb0df9b258a3cd2a821acade65e8ed7
Misael Taveras <hello@taverasmisael.com>
2023-05-21 10:36:23 -0400

    update dependencies
    Add the .astro folder to the gitignore list

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
-// 2. Define your collection(s)
+
 const blogCollection = defineCollection({
-  schema: ({image}) =>
+  type: "content",
+  schema: ({ image }) =>
     z.object({
       isDraft: z.boolean().default(false),
       title: z.string(),
@@ -13,8 +13,7 @@ const blogCollection = defineCollection({
       banner: image(),
     }),
 });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+
 export const collections = {
   blog: blogCollection,
 };
