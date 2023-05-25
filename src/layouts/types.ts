6a7637743b9c2c78c4a6ae2bd24c9cea14b7b67f
Misael Taveras <hello@taverasmisael.com>
2023-05-25 10:46:02 -0400

    add open graphs tags to the pages

--- src/layouts/types.ts
+++ src/layouts/types.ts
@@ -1,8 +1,12 @@
 import type { Language } from '@/utils/i18n';
 import type {AstroBuiltinAttributes} from 'astro'
+
+type OGTypes = "website" | "article" | "blog" | "author";
 export interface DefaultLayoutProps {
   title: string;
   canonical: string;
+  image?: string;
+  ogType?: OGTypes,
   lang?: Language;
   description: string;
   class?: string;
