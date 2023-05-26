f497a1bd18412686db0188e84024319bf5ea1b51
Misael Taveras <hello@taverasmisael.com>
2023-05-26 18:47:29 -0400

    use font source to add new fonts
    I settle, for now with Inter for the body and Red Hat Display for the titles.
    Fira Code is always my favourite monospace.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Inter Variable"', ...defaultTheme.fontFamily.sans],
+        display: ['"Red Hat Display Variable"', ...defaultTheme.fontFamily.serif],
+        mono: ['"Fira Code Variable"', ...defaultTheme.fontFamily.mono],
+      },
       height: {
         100: "30rem",
       },
