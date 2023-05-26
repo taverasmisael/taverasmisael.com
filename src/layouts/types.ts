cb4752ecc810c19dde1dc2c92fdf97b1c4f731b8
Misael Taveras <hello@taverasmisael.com>
2023-05-26 00:19:05 -0400

    move footer and translations to the Basic layout
    I had the translations all over the place so now they are centralized on the Basic layout.
    This also will help with a future task :wink:
    
    The other part is the footer, that was imported on all layouts, moved it to Basic layout.

--- src/layouts/types.ts
+++ src/layouts/types.ts
@@ -1,15 +1,17 @@
-import type { Language } from '@/utils/i18n';
-import type {AstroBuiltinAttributes} from 'astro'
+import type { EntryTranslationReference } from "@/utils/content";
+import type { Language } from "@/utils/i18n";
+import type { AstroBuiltinAttributes } from "astro";
 
 type OGTypes = "website" | "article" | "blog" | "author";
 export interface DefaultLayoutProps {
   title: string;
   canonical: string;
   image?: string;
-  ogType?: OGTypes,
+  ogType?: OGTypes;
   lang?: Language;
   description: string;
   class?: string;
-  "class:list"?: AstroBuiltinAttributes['class:list'];
+  "class:list"?: AstroBuiltinAttributes["class:list"];
   fullHeight?: boolean;
+  translations: EntryTranslationReference[];
 }
