859d7e87c12348867d34ee7e05e77a6004d18420
Misael Taveras <hello@taverasmisael.com>
2023-06-16 10:51:01 -0400

    fix: add search button event listener

--- src/layouts/keyboard-shortcuts.ts
+++ src/layouts/keyboard-shortcuts.ts
@@ -1,6 +1,14 @@
 import { showCommandBar } from "@/stores/command-bar.store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const searchTrigger = document.getElementById("search-trigger");
+  if (searchTrigger) {
+    searchTrigger.addEventListener("click", e => {
+      e.preventDefault();
+      showCommandBar();
+    });
+  }
+
   document.addEventListener("keydown", e => {
     // If target is input type, ignore
     if (
